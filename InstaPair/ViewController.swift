d6feea1ff74c64e9a672233ccef2cc1ce8173e20
leovinh <thevinhduong@gmail.com>
2016-03-30 11:24:43 +0700

    Initial Commit

---
+++ InstaPair/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstaPair
+//
+//  Created by The Vinh Duong on 3/30/16.
+//  Copyright © 2016 Cititech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
