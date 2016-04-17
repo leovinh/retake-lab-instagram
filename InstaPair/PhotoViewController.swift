36d8a9d97e7141a22deb4c93a1be5bdc3072a3ec
leovinh <thevinhduong@gmail.com>
2016-04-17 23:48:49 +0700

    Add pull to request

--- InstaPair/PhotoViewController.swift
+++ InstaPair/PhotoViewController.swift
@@ -23,6 +23,11 @@ class PhotoViewController: UIViewController, UITableViewDelegate, UITableViewDat
         
         fetchData()
         
+        // Initialize a UIRefreshControl
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(PhotoViewController.refreshControlAction(_:)), forControlEvents: UIControlEvents.ValueChanged)
+        tableView.insertSubview(refreshControl, atIndex: 0)
+        
 
     }
 
@@ -85,4 +90,44 @@ class PhotoViewController: UIViewController, UITableViewDelegate, UITableViewDat
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         tableView.deselectRowAtIndexPath(indexPath, animated:true)
     }
+    
+    // Makes a network request to get updated data
+    // Updates the tableView with the new data
+    // Hides the RefreshControl
+    
+    func refreshControlAction(refreshControl: UIRefreshControl) {
+        
+        // ... Create the NSURLRequest (myRequest) ...
+        let clientId = "e05c462ebd86446ea48a5af73769b602"
+        let url = NSURL(string:"https://api.instagram.com/v1/media/popular?client_id=\(clientId)")
+        let request = NSURLRequest(URL: url!)
+        
+        // Configure session so that completion handler is executed on main UI thread
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate:nil,
+            delegateQueue:NSOperationQueue.mainQueue()
+        )
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+                                                                      completionHandler: { (dataOrNil, response, error) in
+                                                                        
+                                                                        // ... Use the new data to update the data source ...
+                                                                        if let data = dataOrNil {
+                                                                            if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                                                                                data, options:[]) as? NSDictionary {
+                                                                                self.mediaData = responseDictionary["data"] as? [NSDictionary]
+                                                                                
+                                                                                // Reload the tableView now that there is new data
+                                                                                self.tableView.reloadData()
+                                                                                
+                                                                                // Tell the refreshControl to stop spinning
+                                                                                refreshControl.endRefreshing()
+                                                                            }
+                                                                        }
+        });
+        task.resume()
+        
     }
+    
+}
